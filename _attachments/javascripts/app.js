ab777bdc789d07bc2082034f8a2e472c1465e19b
Alexander Lang <alex@upstream-berlin.com>
2010-05-24 20:39:24 +0200

    removed slideshow feature

--- _attachments/javascripts/app.js
+++ _attachments/javascripts/app.js
@@ -30,27 +30,11 @@ $(function() {
     this.get('#/slides/:number', function(context) {
       var slide_number = parseInt(context.params['number'], 10);
       store.slide_by_number(context.current_presentation_id(), slide_number, function(slide) {
-        var transformation = slide.transformation;
-        screen.transform_to(transformation);
+        screen.transform_to(slide.transformation);
         $('#current_slide').text(slide_number);
         $('#next_link').attr('href', '#/slides/' + (slide_number + 1));
         $('#previous_link').attr('href', '#/slides/' + (slide_number > 1 ? slide_number - 1 : 1));
-        store.save_slide_view(slide);
       });
-    });
-    
-    this.get('#/slide_views/:number', function(context) {
-      var slide_view_number = parseInt(context.params['number'], 10);
-      store.slide_views(slide_view_number, 2, function(slide_view, next_slide_view) {
-        store.slide(slide_view.slide_id, function(slide) {
-          screen.transform_to(slide.transformation);
-        });
-        var last_view_time = context.last_view_time || new Date();
-        window.setTimeout(function() {
-          location.hash = '/slide_views/' + (slide_view_number + 1);
-        }, new Date(next_slide_view.created_at) - new Date(slide_view.created_at) - (new Date() - last_view_time));
-      });
-      this.last_view_time = new Date();
     });
     
     this.post('#/slides', function(context) {
--- _attachments/javascripts/store.js
+++ _attachments/javascripts/store.js
@@ -12,13 +12,6 @@ Store = function(couchapp) {
         }
       });
     },
-    slide: function(slide_id, callback) {
-      couchapp.db.openDoc(slide_id, {
-        success: function(slide) {
-          callback(slide);
-        }
-      });
-    },
     slide_by_number: function(presentation_id, number, callback) {
       couchapp.design.view('slides', {
         reduce: false,
@@ -33,32 +26,8 @@ Store = function(couchapp) {
         }
       });
     },
-    save_slide_view: function(slide) {
-      couchapp.db.saveDoc({
-          type: 'SlideView',
-          created_at: new Date().toJSON(),
-          slide_id: slide._id
-        }, {
-          success: function(json) {
-        }
-      });
-    },
     save_slide: function(slide, callback) {
       couchapp.db.saveDoc(slide, {success: callback});
-    },
-    slide_views: function(slide_view_number, count, callback) {
-      couchapp.design.view('slide_views', {
-        include_docs: true,
-        limit: count || 1,
-        skip: slide_view_number - 1,
-        success: function(json) {
-          var slide_views = [];
-          for(var i in json['rows']) {
-            slide_views.push(json['rows'][i]['doc']);
-          };
-          callback.apply(null, slide_views);
-        }
-      });
     },
     slide_count: function(presentation_id, callback) {
       couchapp.design.view('slides', {
